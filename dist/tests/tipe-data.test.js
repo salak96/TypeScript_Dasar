04cf7fd45116e45ab9a6ba12d5b6efa4cb4f081a
lambang <sasangkakurniawan.com>
2023-07-21 14:31:30 +0700

    tipe data typeScript

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Tipe Data', () => {
+    it('should must declare', () => {
+        const name = 'Hello Lambang';
+        const balance = 100000;
+        const isVip = true;
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Tipe Data', () => {
+    it('should must declare', () => {
+     const name:string = 'Hello Lambang';
+     const balance:number = 100000;
+     const isVip:boolean = true;
+
+     console.info(name);
+     console.info(balance);
+     console.info(isVip);
+    });
+});
