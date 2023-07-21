9b328a2609a09ccf68f8d719291b8191af84c537
lambang <sasangkakurniawan.com>
2023-07-21 20:18:34 +0700

    tipe data any all

--- tests/any.test.ts
+++ tests/any.test.ts
@@ -2,12 +2,12 @@ describe('Any', () => {
     it('should support in typescript', () => {
         const person: any = {
             id: 1,
-            name: 'Sasangka',
+            name: 'Sasangka',       //tipe data object any apapun tipe bisa dimasukan
             age: 27,
         };
 
-        person.age = 28;
-        person.address = 'Jakarta';
+        person.age = 28;            // bisa di ganti isi data object
+        person.address = 'Jakarta'; // bisa di ganti isi data object
         console.info(person);
     });
 });
