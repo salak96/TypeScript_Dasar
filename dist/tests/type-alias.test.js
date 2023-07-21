f648b4f4f4e1bd574d6f534c4a18861871471ac7
lambang <sasangkakurniawan.com>
2023-07-21 20:48:15 +0700

    alias type tipe data2 union

--- dist/tests/type-alias.test.js
+++ dist/tests/type-alias.test.js
@@ -1,11 +1,11 @@
 describe('Type alias', () => {
     it('should support in typescript', () => {
         const category = {
-            id: '1',
+            id: 1,
             name: 'Handphone', //HARUS ADA id & name
         };
         const product = {
-            id: '1',
+            id: 2,
             name: 'Samsung',
             price: 100000,
             category: category
--- tests/type-alias.test.ts
+++ tests/type-alias.test.ts
@@ -3,12 +3,12 @@ import { Category, Product } from "../src/type-alias"
 describe('Type alias', () => {
     it('should support in typescript', () => {
         const category : Category = {
-            id : '1',
+            id : 1,
             name : 'Handphone',       //HARUS ADA id & name
         }
 
         const product : Product  = {
-            id : '1',
+            id : 2,
             name : 'Samsung',
             price : 100000,
             category : category
