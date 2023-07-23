107fb31e9ed74d4f79b0d34422c364c3eb7ccfbf
lambang <sasangkakurniawan.com>
2023-07-23 16:12:37 +0700

    interface function

--- dist/tests/interface.test.js
+++ dist/tests/interface.test.js
@@ -3,9 +3,21 @@ describe('interface', () => {
         const seller = {
             id: 1,
             name: 'TOKO ABC',
-            address: 'Jakarta'
+            address: 'Jakarta',
+            nib: '132344',
+            npwp: '123456789'
         };
+        seller.name = 'TOKO XYZ'; // bisa di ubah
+        // seller.nib = '1234567891' tidak bisa di ubah
         console.log(seller);
+    });
+    // Function interface
+    it('should support function interface', () => {
+        const add = (value1, value2) => {
+            return value1 + value2;
+        };
+        expect(add(1, 2)).toBe(3);
+        console.log(add(1, 2));
     });
 });
 export {};
--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -12,6 +12,17 @@ describe('interface', () => {
         seller.name = 'TOKO XYZ'    // bisa di ubah
         // seller.nib = '1234567891' tidak bisa di ubah
         console.log(seller);
-
     });
+
+    // Function interface
+    it('should support function interface', () => {
+        interface AddFunction {
+            (value1: number, value2: number): number
+        }
+        const add: AddFunction = (value1: number, value2: number):number => {
+            return value1 + value2
+        };
+        expect(add(1, 2)).toBe(3);
+        console.log(add(1, 2));
+    })
 })
